e5847c6660ea3ebda574ee5879363687fa1984fc
dsapkota <dipta.sapkota@viasat.com>
2017-09-06 14:22:02 -0600

    code added

---
+++ data/friend.js
@@ -0,0 +1,21 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Dsapkota',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
